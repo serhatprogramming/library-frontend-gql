fce7f70023fcba6a8720bcf425427ddf6c0edb44
serhatprogramming <serhatprogramming@gmail.com>
2023-04-27 18:13:23 -0400

    select drop down added for authors

--- src/components/BirthForm.js
+++ src/components/BirthForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // apollo client
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 // queries
 import { EDIT_AUTHOR, GET_AUTHORS } from "../queries";
 
@@ -8,13 +8,24 @@ const BirthForm = () => {
   const [name, setName] = useState("");
   const [bornYear, setBornYear] = useState("");
 
+  const result = useQuery(GET_AUTHORS);
+
   const [editAuthor] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: GET_AUTHORS }],
+    onError: (e) => {
+      console.log("error: ", e);
+    },
   });
 
-  const changeBornYear = (e) => {
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
+
+  const authors = result.data.allAuthors;
+
+  const changeBornYear = async (e) => {
     e.preventDefault();
-    editAuthor({ variables: { name, born: bornYear } });
+    await editAuthor({ variables: { name, born: bornYear } });
     setName("");
     setBornYear("");
   };
@@ -24,12 +35,17 @@ const BirthForm = () => {
       <h2>change birth year</h2>
       <form onSubmit={changeBornYear}>
         <div>
-          name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <label>
+            Pick an author:
+            <select value={name} onChange={(e) => setName(e.target.value)}>
+              <option disabled></option>
+              {authors.map((author) => (
+                <option key={author.name} value={author.name}>
+                  {author.name}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
         <div>
           year:
